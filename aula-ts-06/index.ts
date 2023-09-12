7f0737782392365d2e7a3d96c123544e1c372350
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-09-12 20:44:36 -0300

    exercicios 4 ao 7

---
+++ aula-ts-06/index.ts
@@ -0,0 +1,32 @@
+interface Platform {
+    id: number;
+    name: string;
+}
+
+interface Game {
+    id: number;
+    platform: Platform;
+    title: string;
+    publisher: string;
+    launch?: string | Date;
+}
+
+const game = {
+    id: 1,
+    platform: {
+        id: 1,
+        name: "Playstation"
+    },
+    title: "The Last of Us",
+    publisher: "Naughty Dog",
+    launch: "2013-06-14" // pode ser um Date. É opcional (para jogos ainda não lançados).
+}
+
+const games : Game[] = [game];
+
+function play(game : Game): void{
+    // runs the game
+    console.log(game);
+}
+
+play(game)
